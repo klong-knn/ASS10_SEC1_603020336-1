f4941471ac3362cd6e299d45897a2bdc2c0001ef
klong-knn <54529534+klong-knn@users.noreply.github.com>
2019-10-27 22:08:47 +0700

    ASS10_SEC1_603020336-1
    27/10/2562

---
+++ app/src/main/java/com/example/ass10/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ass10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myweb.Ass10sqlite_sec1.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
